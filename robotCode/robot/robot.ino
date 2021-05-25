cfe14c4b18ea8a1f66aee7bb71571fd69f4ae44a
ming <yuemingtan1996@gmail.com>
2021-05-25 19:56:29 +0200

    robotCode v1

--- robotCode/robot/robot.ino
+++ robotCode/robot/robot.ino
@@ -1,17 +1,13 @@
 //motor 1
-int ln1 = 32;
-int ln2 = 33;
-int ena = 15;
+int ln1 = 0; //D3
+int ln2 = 4; //D2
+int ena = 5; //D1
 
 //motor 2
-int ln3 = 21;
-int ln4 = 22;
-int enb = 14;
+int ln3 = 14; //D5
+int ln4 = 12; //D6
+int enb = 13; //D7
 
-const int freq = 5000;
-const int pwmChannel_1 = 0;
-const int pwmChannel_2 = 0;
-const int resolution = 8;
 
 void setup() 
 {
@@ -23,17 +19,13 @@ void setup()
   pinMode(ln4 , OUTPUT);
   pinMode(enb , OUTPUT);
   
-  ledcSetup(pwmChannel_1, freq, resolution);
-  ledcSetup(pwmChannel_2, freq, resolution);
-  ledcAttachPin(ena, pwmChannel_1);
-  ledcAttachPin(enb, pwmChannel_2);
 
 }
 
 void set_speed(int Speed)
 {
-  ledcWrite(pwmChannel_1, Speed);
-  ledcWrite(pwmChannel_2, Speed);
+ analogWrite(ena,Speed);
+ analogWrite(enb,Speed);
  }
  
 void move_forward(int Speed)
@@ -65,24 +57,24 @@ void motor_off()
 void move_right(int Speed)
 {
   set_speed(Speed);
-  digitalWrite(ln1,HIGH);
-  digitalWrite(ln2 , LOW);
-  digitalWrite(ln3, LOW);
-  digitalWrite(ln4 , HIGH); 
-  }
-  
-void move_left(int Speed)
-{
-  set_speed(Speed);
   digitalWrite(ln1,LOW);
   digitalWrite(ln2 , HIGH);
   digitalWrite(ln3, HIGH);
   digitalWrite(ln4 , LOW); 
   }
-
+  
+void move_left(int Speed)
+{
+   set_speed(Speed);
+  digitalWrite(ln1,HIGH);
+  digitalWrite(ln2 , LOW);
+  digitalWrite(ln3, LOW);
+  digitalWrite(ln4 , HIGH); 
+  }
 
 void loop()
 {
+  // move forward , set speed to 255 out of possible range 0~255
   move_forward(255);
   delay(3000);
   motor_off();
@@ -99,4 +91,5 @@ void loop()
   delay(3000);
   motor_off();
   delay(3000);
+
 }
