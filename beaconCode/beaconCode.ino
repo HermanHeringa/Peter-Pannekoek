8bc94811d776a60effee922b368016994b402c11
HermanHeringa <hermanheringa@hotmail.com>
2021-05-25 15:03:35 +0200

    added UDP connectivity WIP

--- beaconCode/beaconCode.ino
+++ beaconCode/beaconCode.ino
@@ -1,14 +1,20 @@
 #include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
 
-String hostSSID = "testtest";
-String hostPass = "88888888";
-String droneIdentifier = "ESP-";
+#define HOST_SSID "testtest";
+#define HOST_PASS "88888888";
+#define DRONE-ID "ESP-";
+#define UDP_PORT 4210
+
+WiFiUDP UDP;
+char packet[255];
+char reply[] = "Packet received!";
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   delay(500);
-  WiFi.begin(hostSSID, hostPass);
+  WiFi.begin(HOST_SSID, HOST_PASS);
 
   Serial.print("\nConnecting");
   while (WiFi.status() != WL_CONNECTED)
@@ -20,13 +26,21 @@ void setup() {
 
   Serial.print("Connected, IP address: ");
   Serial.println(WiFi.localIP());
+
+  UDP.begin(UDP_PORT);
+  Serial.print("Listening on UDP port ");
+  Serial.println(UDP_PORT);
 }
 
 void loop() {
-   int n = WiFi.scanNetworks();
+  
+  int n = WiFi.scanNetworks();
   for (int i = 0; i < n; i++)
   {
-    if (strstr(WiFi.SSID(i).c_str(), droneIdentifier)) {
+    if (strstr(WiFi.SSID(i).c_str(), DRONE-ID)) {
+      UDP.beginPacket(UDP.remoteIP(), UDP.remotePort());
+      UDP.write("%s (%ddBm)\n", WiFi.SSID(i).c_str(), WiFi.RSSI(i));
+      UDP.endPacket();
       Serial.printf("%s (%ddBm)\n", WiFi.SSID(i).c_str(), WiFi.RSSI(i));
     }
   }
--- robotConnectionCode/robotConnectionCode.ino
+++ robotConnectionCode/robotConnectionCode.ino
@@ -1,14 +1,21 @@
 #include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
 
-String hostSSID = "testtest";
-String hostPass = "88888888";
-String mySSID = "ESP-0";
-String myPass = "2xjYrA2Ny3VwyJuh";
+#define HOST_SSID "testtest";
+#define HOST_PASS "88888888";
+#define MY_SSID "ESP-0";
+#define MY_PASS "2xjYrA2Ny3VwyJuh";
+#define UDP_PORT 4210
+#define HOST_IP "192.168.137.1"
+
+WiFiUDP UDP;
+char packet[255];
+char reply[] = "Packet received!";
 
 void setup() {
-  // put your setup code here, to run once:
+  // Connect to host wifi network
   Serial.begin(9600);
-  WiFi.begin(hostSSID, hostPass);
+  WiFi.begin(HOST_SSID, HOST_PASS);
   Serial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -19,14 +26,34 @@ void setup() {
   Serial.print("Connected, IP address: ");
   Serial.println(WiFi.localIP());
 
-  boolean fakeNetwork = WiFi.softAP(mySSID, MyPass);
+  // Setup network for beacons to track
+  boolean fakeNetwork = WiFi.softAP(MY_SSID, MY_PASS);
   if (fakeNetwork) {
     Serial.println("Network created.");
   }
   else {
     Serial.println("Network creation failed");
   }
+
+  // Setup UDP
+  UDP.begin(UDP_PORT);
+  Serial.print("Listening on UDP port ");
+  Serial.println(UDP_PORT);
 }
 
 void loop() {
+  int packetSize = UDP.parsePacket();
+  if (packetSize) {
+    Serial.print("Received packet! Size: ");
+    Serial.println(packetSize); 
+    int len = UDP.read(packet, 255);
+    if (len > 0)
+    {
+      packet[len] = '\0';
+    }
+    Serial.print("Packet received: ");
+    Serial.println(packet);
+  }
+
+  // driving code here
 } 
