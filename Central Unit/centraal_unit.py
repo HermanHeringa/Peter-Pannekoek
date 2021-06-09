7f5aa69b2527217a465783e33f6f93f181eef65e
Martijn van Adrichem <0932927@hr.nl>
2021-06-09 17:53:24 +0200

    distance functie toegevoegd aan centraal_unit.py

--- Central Unit/centraal_unit.py
+++ Central Unit/centraal_unit.py
@@ -1,10 +1,24 @@
-import keyboard 
+#import keyboard 
 import time
+import math
 from myUDP import UDPsender,UDPreceiver
 
 packet = UDPsender("192.168.43.70",4210) # Network ip and port
 fromRobot_1 = UDPreceiver("224.0.1.3",4210)# Host_ip in arduino code, each robot has its own host IP
 fromRobot_1.receive()
+
+def calculate_distance(pos1, pos2):
+        x1 = pos1[0]
+        x2 = pos2[0]
+        y1 = pos1[1]
+        y2 = pos2[1]
+        distance = math.sqrt(((x2-x1)**2)+((y2-y1)**2))
+        return distance
+
+#calculate_distance test, output should be 1.41 with pos1[2,2] and pos2[3,3]
+#pos1 = [2,2]
+#pos2 = [3,3]
+#print(calculate_distance(pos1, pos2))
 
 while True:
     if keyboard.is_pressed('UP'): 
