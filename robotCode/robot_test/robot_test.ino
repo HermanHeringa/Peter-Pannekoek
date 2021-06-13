6b4309783b8e1c716dd2303960f9c58f9fcad073
ming <yuemingtan1996@gmail.com>
2021-06-13 21:09:47 +0200

    robot kan op de juiste hoek draaien

--- robotCode/robot_test/robot_test.ino
+++ robotCode/robot_test/robot_test.ino
@@ -1,19 +1,13 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_HMC5883_U.h>
+#include <HMC5883L.h>
+HMC5883L compass;
+
 #define HOST_SSID "AndroidAP"
 #define HOST_PASS "pleo9996"
-#define MY_SSID "ESP-14"
-#define MY_PASS "2xjYrA2Ny3VwyJuh"
 #define UDP_PORT 4210
 IPAddress HOST_IP =IPAddress(224, 0, 1, 3);
-
-
-/* Assign a unique ID to this sensor at the same time */
-Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
-
 
 //motor 1
 int motorOneReverse = 0; //D3
@@ -28,18 +22,12 @@ int motorTwoSpeed = 13; //D7
 WiFiUDP UDP;
 char packet[255];
 char reply[] = "Packet received!";
+int current_angle = 0;
 
 
 void setup() 
 {
-  /* Initialise the sensor */
-  if(!mag.begin())
-  {
-    /* There was a problem detecting the HMC5883 ... check your connections */
-    Serial.println("Ooops, no HMC5883 detected ... Check your wiring!");
-    while(1);
-  }
-  
+  //setup motors
   pinMode(motorOneReverse, OUTPUT);
   pinMode(motorOneForward , OUTPUT);
   pinMode(motorOneSpeed , OUTPUT);
@@ -61,31 +49,122 @@ void setup()
   Serial.print("Connected, IP address: ");
   Serial.println(WiFi.localIP());
 
-  // Setup network for beacons to track
-  boolean fakeNetwork = WiFi.softAP(MY_SSID, MY_PASS);
-  if (fakeNetwork) {
-    Serial.println("Network created.");
-  }
-  else {
-    Serial.println("Network creation failed");
-  }
 
   // Setup UDP
   UDP.begin(UDP_PORT);
   Serial.print("Listening on UDP port ");
   Serial.println(UDP_PORT);
+
+  // Initialize Initialize HMC5883L
+  Serial.println("Initialize HMC5883L");
+  while (!compass.begin())
+  {
+    Serial.println("Could not find a valid HMC5883L sensor, check wiring!");
+    delay(500);
+  }
+
+  // Set measurement range
+  compass.setRange(HMC5883L_RANGE_1_3GA);
+
+  // Set measurement mode
+  compass.setMeasurementMode(HMC5883L_CONTINOUS);
+
+  // Set data rate
+  compass.setDataRate(HMC5883L_DATARATE_30HZ);
+
+  // Set number of samples averaged
+  compass.setSamples(HMC5883L_SAMPLES_8);
+
+  // Set calibration offset. See HMC5883L_calibration.ino
+  compass.setOffset(128, -158);
+
+  
+  }
+
+//main loop
+void loop()
+{
+   delay(5000);
+  rotate("right", 180);
+  delay(2000);
+  move_forward(200);
+  delay(4000);
+  rotate("right", 90);
+  move_forward(200);
+  delay(2500);
+  rotate("right", 90);
+  delay(1500);
+   move_forward(200);
+  delay(2500);
+  rotate("right", 90);
+  delay(2500);
+  move_forward(200);
+  delay(5000);
+  rotate("right", 270);
+  delay(4000);
+  move_forward(200);
+  delay(3000);
+  motor_off();
+    
+ //commandFromCU();
+
 }
 
-void loop(void)
-{
+void rotate( int angle){
 
-    getAngle();
-    int packetSize = UDP.parsePacket();
+    current_angle = getAngle();
+    int target_angle = current_angle + angle;
+    
+    if (target_angle > 360){
+      target_angle = target_angle % 360;
+    }
+   
+    while(getAngle() != target_angle){
+      move_right(170);
+      delay(50);
+    }
+      motor_off();
+}
 
+//get current angle
+int getAngle(){
+ Vector norm = compass.readNormalize();
 
+  // Calculate heading
+  float heading = atan2(norm.YAxis, norm.XAxis);
+
+  // Set declination angle on your location and fix heading
+  // You can find your declination on: http://magnetic-declination.com/
+  // (+) Positive or (-) for negative
+  // For Bytom / Poland declination angle is 4'26E (positive)
+  // Formula: (deg + (min / 60.0)) / (180 / M_PI);
+  float declinationAngle = (1.0 + (49.0 / 60.0)) / (180 / M_PI);//rotterdam
+  heading += declinationAngle;
+
+  // Correct for heading < 0deg and heading > 360deg
+  if (heading < 0)
+  {
+    heading += 2 * PI;
+  }
+
+  if (heading > 2 * PI)
+  {
+    heading -= 2 * PI;
+  }
+
+  // Convert to degrees
+  float headingDegrees = heading * 180/M_PI; 
+  headingDegrees = (360 + (int)headingDegrees - 95) % 360;
+
+  return headingDegrees;
+}
+
+//receive commands from Central Unit
+void commandFromCU(){
+  int packetSize = UDP.parsePacket();
   if (packetSize) {
-    Serial.print("Received packet! Size: ");
-    Serial.println(packetSize); 
+    //Serial.print("Received packet! Size: ");
+    //Serial.println(packetSize); 
     int len = UDP.read(packet, 255);
     if (len > 0)
     {
@@ -124,47 +203,23 @@ void loop(void)
   }
 }
 
+//to send packet to Central unit
 void sendPacket(String packet){
     String string= packet;
     char msg[255];
-    string.toCharArray(msg,255);
+    string.toCharArray(msg,255);//convert string to char array
     UDP.beginPacketMulticast(HOST_IP, UDP_PORT, WiFi.localIP());
     UDP.write(msg);
     UDP.endPacket();
 }
 
-
-void getAngle(){
-  /* Get a new sensor event */ 
-  sensors_event_t event; 
-  mag.getEvent(&event);
-
-  float heading = atan2(event.magnetic.y, event.magnetic.x);
-  float declinationAngle = 0.22;
-  heading += declinationAngle;
- 
-  // Correct for when signs are reversed.
-  if(heading < 0)
-    heading += 2*PI;
- 
-  // Check for wrap due to addition of declination.
-  if(heading > 2*PI)
-    heading -= 2*PI;
- 
-  // Convert radians to degrees for readability.
-  float headingDegrees = heading * 180/M_PI; 
-  String HDegrees = String(headingDegrees);
-  Serial.println(HDegrees);
-  sendPacket(HDegrees);
- 
-  delay(100);
-}
-
+// set motor speed range 200-255
 void set_speed(int Speed) {
   analogWrite(motorOneSpeed,Speed);
   analogWrite(motorTwoSpeed,Speed);
 }
- 
+
+//move forward
 void move_forward(int speed) { 
   set_speed(speed);
   digitalWrite(motorOneReverse, LOW);
@@ -172,7 +227,8 @@ void move_forward(int speed) {
   digitalWrite(motorTwoReverse, LOW);
   digitalWrite(motorTwoForward , HIGH);
 }
-  
+
+//move back
 void move_back(int speed) {
   set_speed(speed);
   digitalWrite(motorOneReverse, HIGH);
@@ -180,14 +236,8 @@ void move_back(int speed) {
   digitalWrite(motorTwoReverse, HIGH);
   digitalWrite(motorTwoForward , LOW);
 }
-  
-void motor_off() {
-  digitalWrite(motorOneReverse,LOW);
-  digitalWrite(motorOneForward , LOW);
-  digitalWrite(motorTwoReverse, LOW);
-  digitalWrite(motorTwoForward , LOW); 
-}
-  
+
+//rotate right
 void move_right(int speed) {
   set_speed(speed);
   digitalWrite(motorOneReverse,LOW);
@@ -195,11 +245,20 @@ void move_right(int speed) {
   digitalWrite(motorTwoReverse, HIGH);
   digitalWrite(motorTwoForward , LOW); 
 }
-  
+
+//rotate left
 void move_left(int speed) {
   set_speed(speed);
   digitalWrite(motorOneReverse,HIGH);
   digitalWrite(motorOneForward , LOW);
   digitalWrite(motorTwoReverse, LOW);
   digitalWrite(motorTwoForward , HIGH); 
+}
+
+//don't move
+void motor_off() {
+  digitalWrite(motorOneReverse,LOW);
+  digitalWrite(motorOneForward , LOW);
+  digitalWrite(motorTwoReverse, LOW);
+  digitalWrite(motorTwoForward , LOW); 
 }
